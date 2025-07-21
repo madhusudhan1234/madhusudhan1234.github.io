ea34e3812db7210a32d1c3a765340bf487f33a56
madhusudhan1234 <madhusudhansubedi4@gmail.com>
2025-07-21 22:57:04 +0900

    new design

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://madhusudhansubedi.com.np',
   integrations: [mdx(), sitemap()],
   adapter: cloudflare(),
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Madhu Sudhan Subedi';
+export const SITE_TITLE = 'Madhu Sudhan Subedi - Software Engineer';
 export const SITE_DESCRIPTION =
-  'Building Knowledge, Inspiring Innovation, Empowering Change.';
+  'Personal website and blog of Madhu Sudhan Subedi, a software engineer from Nepal based in Japan.';
