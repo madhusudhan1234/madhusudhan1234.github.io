fed8991fd0f2745a728c2a1d4b6768215111e1fd
madhusudhan1234 <madhusudhansubedi4@gmail.com>
2025-11-01 17:49:09 +0900

    episode 33

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://madhusudhansubedi.com.np',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react(), tailwind()],
   adapter: cloudflare(),
 });
