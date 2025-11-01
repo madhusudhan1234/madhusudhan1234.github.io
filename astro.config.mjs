8b42f5c4ee465bc1b9b95db6874fa3a9db7ab941
madhusudhan1234 <madhusudhansubedi4@gmail.com>
2025-11-01 18:01:12 +0900

    Fixing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
 
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://madhusudhansubedi.com.np',
-  integrations: [mdx(), sitemap(), react(), tailwind()],
+  integrations: [mdx(), sitemap()],
   adapter: cloudflare(),
 });
