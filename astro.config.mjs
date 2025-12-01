ff0c54764b131012ab90f9530383811c78a71dcc
madhusudhan1234 <madhusudhansubedi4@gmail.com>
2025-12-01 23:48:32 +0900

    build testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://madhusudhansubedi.com.np',
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare(),
+  output: 'static',
 });
